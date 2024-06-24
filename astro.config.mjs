19eac46546c663f5e9da87e15bd4f5020e1cbbbd
Lukáš Novoroľník <lukas.novorolnik@gmail.com>
2024-06-24 11:34:26 +0200

    new sitemap settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,22 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.sasz.sk",
-  integrations: [mdx(), react(), sitemap()],
+  integrations: [
+    mdx(),
+    react(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+      entryLimit: 10000,
+      serialize(item) {
+        if (item.url === "/") {
+          return { ...item, priority: 1.0 };
+        }
+        return item;
+      },
+    }),
+  ],
   components: {
     "@astro-components/embed-youtube": "astro-embed-youtube",
   },
